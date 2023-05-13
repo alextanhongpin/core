961625883b28e9bb0124f3c864919582d4638262
alextanhongpin <alextan220990@gmail.com>
2023-05-13 15:09:18 +0800

    chore: add snapshottest package

---
+++ test/snapshottest/README.md
@@ -0,0 +1,5 @@
+# Snapshot Testing
+
+Snapshot testing is a type of software testing that compares the output of a software application to a known good state, or snapshot. This is done by taking a snapshot of the application's output at a specific point in time, and then comparing it to the snapshot at a later point in time. If the two snapshots are not identical, then the application has changed in an unexpected way, and a test failure is reported.
+
+Snapshot testing is a valuable tool for ensuring that software applications do not change in an unexpected way. It can be used to detect changes in the application's output, UI, behavior, and data. Snapshot testing is often used in conjunction with other types of software testing, such as unit testing and integration testing.
---
+++ test/snapshottest/snapshottest_test.go
@@ -0,0 +1,83 @@
+package snapshottest_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/alextanhongpin/core/test/snapshottest"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func TestSnapshotTest(t *testing.T) {
+	type Person struct {
+		Name      string    `json:"name"`
+		Age       int64     `json:"age"`
+		IsMarried bool      `json:"isMarried"`
+		BornAt    time.Time `json:"bornAt"`
+	}
+
+	p := Person{
+		Name:      "John Appleseed",
+		Age:       13,
+		IsMarried: true,
+		BornAt:    time.Now(),
+	}
+
+	// To skip field comparison, return true.
+	// This is useful for undeterministic values such ad
+	// date time.
+	opt := cmpopts.IgnoreMapEntries(func(k string, v any) bool {
+		if k == "bornAt" {
+			// We just check the expected type can be marshalled
+			// to golang's time.
+			return snapshottest.IsJsonTime(t, v)
+		}
+
+		// Don't skip by default.
+		return false
+	})
+	// It is recommended to keep the snapshot data in
+	// testdata directory.
+	snapshottest.Snapshot(t, p, "./testdata/person.json", opt)
+}
+
+func TestHTTPSnapshotTest(t *testing.T) {
+	type Person struct {
+		Name      string    `json:"name"`
+		Age       int64     `json:"age"`
+		IsMarried bool      `json:"isMarried"`
+		BornAt    time.Time `json:"bornAt"`
+	}
+
+	p := Person{
+		Name:      "John Appleseed",
+		Age:       13,
+		IsMarried: true,
+		BornAt:    time.Now(),
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+	r := httptest.NewRequest("GET", "/users/1", nil)
+
+	opt := cmpopts.IgnoreMapEntries(func(k string, v any) bool {
+		if k == "bornAt" {
+			// We just check the expected type can be marshalled
+			// to golang's time.
+			return snapshottest.IsJsonTime(t, v)
+		}
+
+		// Don't skip by default.
+		return false
+	})
+
+	out := "./testdata/get_user_response.json"
+	snapshottest.HTTPSnapshot(t, r, handler, out, http.StatusOK, opt)
+}
