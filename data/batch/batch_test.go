afe9ac6ed31e01f65ad0014ff9bff5b638fbc87d
alextanhongpin <alextan220990@gmail.com>
2023-07-04 01:26:58 +0800

    chore: update example

--- data/batch/batch_test.go
+++ data/batch/batch_test.go
@@ -8,6 +8,126 @@ import (
 	"github.com/alextanhongpin/core/data/batch"
 )
 
+func ExampleOneToOne() {
+	// Book belongs to an Author.
+	type Author struct {
+		ID   int
+		Name string
+	}
+
+	type Book struct {
+		ID       int
+		AuthorID int
+		Author   *Author
+	}
+
+	batchFn := func(authorIds ...int) ([]Author, error) {
+		authors := make([]Author, len(authorIds))
+		for i, id := range authorIds {
+			authors[i] = Author{
+				ID:   id,
+				Name: fmt.Sprintf("author of book %d", id),
+			}
+		}
+		return authors, nil
+	}
+
+	keyFn := func(a Author) (authorID int, err error) {
+		authorID = a.ID
+		return
+	}
+
+	loader := batch.NewLoader(batchFn, keyFn)
+
+	// We have a bunch of books, and we want to load the author.
+	books := []Book{
+		{ID: 1, AuthorID: 1},
+		{ID: 2, AuthorID: 1}, // Same author as Book ID 1.
+		{ID: 3, AuthorID: 2},
+	}
+
+	for i := 0; i < len(books); i++ {
+		// Create a non-nil Author.
+		books[i].Author = new(Author)
+
+		// Load and assign Author to Book.
+		loader.Load(books[i].AuthorID, books[i].Author)
+	}
+
+	// Initiate the fetch.
+	if err := loader.Wait(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(books[0].Author.Name)
+	fmt.Println(books[1].Author.Name)
+	fmt.Println(books[2].Author.Name)
+	// Output:
+	// author of book 1
+	// author of book 1
+	// author of book 2
+}
+
+func ExampleOneToMany() {
+	// An Author has many Books.
+	type Book struct {
+		ID       int
+		AuthorID int
+	}
+
+	type Author struct {
+		ID    int
+		Books []Book
+	}
+
+	batchFn := func(authorIds ...int) ([]Book, error) {
+		var books []Book
+		for _, id := range authorIds {
+			// The number of books is proportional to the AuthorID.
+			// AuthorID 0 will 0 books.
+			// AuthorID 1 will 1 book.
+			// AuthorID n will n books.
+			for j := 0; j < id; j++ {
+				books = append(books, Book{
+					ID:       j,
+					AuthorID: id,
+				})
+			}
+		}
+
+		return books, nil
+	}
+
+	keyFn := func(b Book) (authorID int, err error) {
+		authorID = b.AuthorID
+		return
+	}
+
+	loader := batch.NewLoader(batchFn, keyFn)
+
+	// We have a bunch of books, and we want to load the author.
+	authors := make([]Author, 3)
+	for i := 0; i < len(authors); i++ {
+		authors[i].ID = i
+
+		// Load and assign Books to Author.
+		loader.LoadMany(authors[i].ID, &authors[i].Books)
+	}
+
+	// Initiate the fetch.
+	if err := loader.Wait(); err != nil {
+		panic(err)
+	}
+
+	for i := range authors {
+		fmt.Printf("author %d has %d books\n", authors[i].ID, len(authors[i].Books))
+	}
+	// Output:
+	// author 0 has 0 books
+	// author 1 has 1 books
+	// author 2 has 2 books
+}
+
 func TestLoad(t *testing.T) {
 	l := newUserLoader()
 
