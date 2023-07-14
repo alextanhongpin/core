6b59ed33c3da469994fba38fe57467d79b62af96
alextanhongpin <alextan220990@gmail.com>
2023-07-15 02:01:56 +0800

    docs: update documentation

--- types/result/result.go
+++ types/result/result.go
@@ -1,4 +1,11 @@
-// package result wraps both data and error together.
+// package result wraps both data and error together. This is useful when
+// passing data through channels.
+//
+// Ideally, each package that needs to have their own result type should define
+// their own result type.
+//
+// Another form of Result is using interface, where instead of just wrapping
+// data, an entire struct can be returned that fulfils the Result interface.
 package result
 
 type Result[T any] struct {
