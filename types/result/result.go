6189194d80dbf9832ef2c5a2c72a5ba2465bdc33
alextanhongpin <alextan220990@gmail.com>
2023-07-21 21:07:22 +0800

    chore(types/result): update result type

--- types/result/result.go
+++ types/result/result.go
@@ -4,21 +4,72 @@
 // Ideally, each package that needs to have their own result type should define
 // their own result type.
 //
-// Another form of Result is using interface, where instead of just wrapping
-// data, an entire struct can be returned that fulfils the Result interface.
+// Another form of result is using interface, where instead of just wrapping
+// data, an entire struct can be returned that fulfils the result interface.
 package result
 
-// Resultable yields result.
+import "errors"
 
-type Resultable[T any] interface {
+var Zero = errors.New("result: no result")
+
+type Result[T any] interface {
 	Unwrap() (T, error)
 }
 
-type Result[T any] struct {
-	Data T
-	Err  error
+type result[T any] struct {
+	data T
+	err  error
 }
 
-func (r *Result[T]) Unwrap() (T, error) {
-	return r.Data, r.Err
+func (r *result[T]) Unwrap() (T, error) {
+	return r.data, r.err
+}
+
+func OK[T any](v T) Result[T] {
+	return &result[T]{
+		data: v,
+	}
+}
+
+func Err[T any](err error) Result[T] {
+	return &result[T]{
+		err: err,
+	}
+}
+
+func All[T any](rs ...result[T]) ([]T, error) {
+	if len(rs) == 0 {
+		return nil, Zero
+	}
+
+	res := make([]T, len(rs))
+	for i, r := range rs {
+		v, err := r.Unwrap()
+		if err != nil {
+			return nil, err
+		}
+		res[i] = v
+	}
+
+	return res, nil
+}
+
+func Any[T any](rs ...result[T]) (T, error) {
+	if len(rs) == 0 {
+		var t T
+		return t, Zero
+	}
+
+	res := make([]error, len(rs))
+	for i, r := range rs {
+		v, err := r.Unwrap()
+		if err == nil {
+			return v, nil
+		}
+
+		res[i] = err
+	}
+
+	var t T
+	return t, errors.Join(res...)
 }
