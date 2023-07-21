25ba9c87645f50217f707dad23468728f7be6f90
alextanhongpin <alextan220990@gmail.com>
2023-07-21 20:57:20 +0800

    feat(sync/future): add future

---
+++ sync/futures/examples_new_test.go
@@ -0,0 +1,42 @@
+package futures_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/alextanhongpin/core/sync/futures"
+)
+
+func ExampleFunc() {
+	f0 := futures.Func[int](func(ctx context.Context) (int, error) {
+		time.Sleep(100 * time.Millisecond)
+		return 42, nil
+	})
+	f1 := futures.Func[int](func(ctx context.Context) (int, error) {
+		time.Sleep(100 * time.Millisecond)
+		return 3173, nil
+	})
+
+	now := time.Now()
+	res := futures.Join(context.Background(), f0, f1)
+	isWithinSLA := time.Since(now) < 110*time.Millisecond
+
+	meaningOfLife, err := res[0].Unwrap()
+	if err != nil {
+		panic(err)
+	}
+
+	elie, err := res[1].Unwrap()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(isWithinSLA)
+	fmt.Println(meaningOfLife)
+	fmt.Println(elie)
+	// Output
+	// true
+	// 42
+	// 3173
+}
