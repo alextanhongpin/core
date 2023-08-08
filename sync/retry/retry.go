0e8adb1170f80cb41c7d8b1ce9c0d994f87c25d0
alextanhongpin <alextan220990@gmail.com>
2023-08-09 00:08:08 +0800

    chore(sync/retry): remove association with generics

--- sync/retry/retry.go
+++ sync/retry/retry.go
@@ -5,41 +5,15 @@ import (
 	"time"
 )
 
-func Linear[T any]() Timeouts[T] {
-	n := 10
+var (
+	Linear      = linear()
+	Exponential = exponential()
+)
 
-	res := make([]time.Duration, n)
-	for i := 0; i < n; i++ {
-		res[i] = 1 * time.Second
-	}
+func Exec(fn func() error, ts []time.Duration) (err error) {
+	timeouts := append([]time.Duration{0}, ts...)
 
-	return res
-}
-
-func Exponential[T any]() Timeouts[T] {
-	n := 10
-
-	res := make([]time.Duration, n)
-	res[0] = 50 * time.Millisecond
-	for i := 1; i < n; i++ {
-		res[i] = jitter(res[i-1] * 2)
-	}
-
-	return res
-}
-
-func jitter(d time.Duration) time.Duration {
-	return (time.Duration(rand.Intn(int(d/2))) + d).Round(5 * time.Millisecond)
-}
-
-type Timeouts[T any] []time.Duration
-
-func (ts Timeouts[T]) timeouts() []time.Duration {
-	return append([]time.Duration{0}, ts...)
-}
-
-func (ts Timeouts[T]) Exec(fn func() error) (err error) {
-	for _, t := range ts.timeouts() {
+	for _, t := range timeouts {
 		if t != 0 {
 			time.Sleep(jitter(t))
 		}
@@ -53,8 +27,10 @@ func (ts Timeouts[T]) Exec(fn func() error) (err error) {
 	return
 }
 
-func (ts Timeouts[T]) Query(fn func() (T, error)) (v T, err error) {
-	for _, t := range ts.timeouts() {
+func Query[T any](fn func() (T, error), ts ...time.Duration) (v T, err error) {
+	timeouts := append([]time.Duration{0}, ts...)
+
+	for _, t := range timeouts {
 		if t != 0 {
 			time.Sleep(jitter(t))
 		}
@@ -66,4 +42,31 @@ func (ts Timeouts[T]) Query(fn func() (T, error)) (v T, err error) {
 	}
 
 	return
+}
+
+func linear() []time.Duration {
+	n := 10
+
+	res := make([]time.Duration, n)
+	for i := 0; i < n; i++ {
+		res[i] = 1 * time.Second
+	}
+
+	return res
+}
+
+func exponential() []time.Duration {
+	n := 10
+
+	res := make([]time.Duration, n)
+	res[0] = 50 * time.Millisecond
+	for i := 1; i < n; i++ {
+		res[i] = jitter(res[i-1] * 2)
+	}
+
+	return res
+}
+
+func jitter(d time.Duration) time.Duration {
+	return (time.Duration(rand.Intn(int(d/2))) + d).Round(5 * time.Millisecond)
 }
