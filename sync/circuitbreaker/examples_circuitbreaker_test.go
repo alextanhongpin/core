0cbc79cbb25479d92371e3a7fe6b294f89349585
alextanhongpin <alextan220990@gmail.com>
2023-10-10 01:13:00 +0800

    chore(sync/circuitbreaker): add example

---
+++ sync/circuitbreaker/examples_circuitbreaker_test.go
@@ -0,0 +1,43 @@
+package circuitbreaker_test
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/alextanhongpin/core/sync/circuitbreaker"
+)
+
+func ExampleCircuitBreaker() {
+	opt := circuitbreaker.NewOption()
+	opt.BreakDuration = 100 * time.Millisecond
+	opt.SamplingDuration = 1 * time.Second
+	opt.OnStateChanged = func(from, to circuitbreaker.Status) {
+		fmt.Printf("status changed from %s to %s\n", from, to)
+	}
+
+	cb := circuitbreaker.New(opt)
+	fmt.Println("initial status:", cb.Status())
+
+	// Opens after failure ratio exceeded.
+	for i := 0; i <= int(opt.FailureThreshold+1); i++ {
+		_ = cb.Do(func() error {
+			return errors.New("foo")
+		})
+	}
+
+	// Break duration.
+	time.Sleep(105 * time.Millisecond)
+
+	// Recover.
+	for i := 0; i <= int(opt.SuccessThreshold+1); i++ {
+		_ = cb.Do(func() error {
+			return nil
+		})
+	}
+	// Output:
+	// initial status: closed
+	// status changed from closed to open
+	// status changed from open to half-open
+	// status changed from half-open to closed
+}
