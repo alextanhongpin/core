fb77d53bf9d755cefc802ac1e75c1837879387b2
alextanhongpin <alextan220990@gmail.com>
2024-01-10 02:09:04 +0800

    refactor: remove text hook as it is no longer used

--- test/testutil/text.go
+++ test/testutil/text.go
@@ -15,13 +15,10 @@ func DumpText(t *testing.T, s string, opts ...TextOption) {
 	t.Helper()
 
 	var fileName string
-	var hooks []testdump.Hook[string]
 	for _, opt := range opts {
 		switch o := opt.(type) {
 		case FileName:
 			fileName = string(o)
-		case *textHookOption:
-			hooks = append(hooks, o.hook)
 		default:
 			panic(fmt.Errorf("testutil: unhandled text option: %#v", opt))
 		}
@@ -36,23 +33,5 @@ func DumpText(t *testing.T, s string, opts ...TextOption) {
 
 	if err := testdump.Text(testdump.NewFile(p.String()), s); err != nil {
 		t.Fatal(err)
-	}
-}
-
-type textHookOption struct {
-	hook testdump.Hook[string]
-}
-
-func (*textHookOption) isText() {}
-
-func InspectText(hook func(snapshot, received string) error) *textHookOption {
-	return &textHookOption{
-		hook: testdump.CompareHook(hook),
-	}
-}
-
-func InterceptText(hook func(dump string) (string, error)) *textHookOption {
-	return &textHookOption{
-		hook: testdump.MarshalHook(hook),
 	}
 }
