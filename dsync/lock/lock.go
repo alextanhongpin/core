760b4524993be34a92bed8791d8f5133c710bf73
alextanhongpin <alextan220990@gmail.com>
2024-08-05 22:01:41 +0800

    refactor(dsync/lock): handle cancellation

--- dsync/lock/lock.go
+++ dsync/lock/lock.go
@@ -3,6 +3,7 @@ package lock
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -17,7 +18,9 @@ var (
 
 	ErrLockReleased = errors.New("lock: already released")
 
-	ErrLockWaitTimeout = errors.New("lock: wait timeout exceeded")
+	ErrLockWaitTimeout = errors.New("lock: lock wait exceeded")
+
+	ErrDone = errors.New("lock: done")
 )
 
 // Locker represents a distributed lock implementation using Redis.
@@ -33,29 +36,34 @@ func New(client *redis.Client) *Locker {
 }
 
 // Lock locks the given key until the function completes.
-// If the lock cannot be acquired within the given timeout, it will error.
+// If the lock cannot be acquired within the given wait, it will error.
 // The lock is released after the function completes.
-func (l *Locker) Lock(ctx context.Context, key string, ttl, timeout time.Duration, fn func(ctx context.Context) error) (err error) {
+func (l *Locker) Lock(ctx context.Context, key string, ttl, wait time.Duration, fn func(ctx context.Context) error) (err error) {
 	// Generate a random uuid as the lock value.
 	val := uuid.New().String()
-	if err := l.lockWait(ctx, key, val, ttl, timeout); err != nil {
+	if err := l.lockWait(ctx, key, val, ttl, wait); err != nil {
 		return err
 	}
 
-	defer func() {
-		err = errors.Join(err, l.unlock(context.WithoutCancel(ctx), key, val))
-	}()
+	defer l.unlock(context.WithoutCancel(ctx), key, val)
 
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer cancel(ErrDone)
 
 	g, ctx := errgroup.WithContext(ctx)
 	g.Go(func() error {
-		return l.refresh(ctx, key, val, ttl)
+		err := l.refresh(ctx, key, val, ttl)
+		// Ignore if the fn is completed.
+		if errors.Is(err, ErrDone) {
+			return nil
+		}
+
+		return err
 	})
 
 	g.Go(func() error {
-		defer cancel()
+		// Signal the refresh to stop.
+		defer cancel(ErrDone)
 
 		return fn(ctx)
 	})
@@ -65,14 +73,14 @@ func (l *Locker) Lock(ctx context.Context, key string, ttl, timeout time.Duratio
 
 // lockWait attempts to acquire the lock. If the lock is already acquired, it
 // will wait for the lock to be released.
-// If the timeout is less than or equal to 0, it will not wait.
-func (l *Locker) lockWait(ctx context.Context, key, val string, ttl, timeout time.Duration) error {
-	// No wait
-	if timeout <= 0 {
+// If the wait is less than or equal to 0, it will not wait.
+func (l *Locker) lockWait(ctx context.Context, key, val string, ttl, wait time.Duration) error {
+	noWait := wait <= 0
+	if noWait {
 		return l.lock(ctx, key, val, ttl)
 	}
 
-	ctx, cancel := context.WithTimeoutCause(ctx, timeout, ErrLockWaitTimeout)
+	ctx, cancel := context.WithTimeoutCause(ctx, wait, ErrLockWaitTimeout)
 	defer cancel()
 
 	for {
@@ -114,7 +122,10 @@ func (l *Locker) refresh(ctx context.Context, key, val string, ttl time.Duration
 
 func (l *Locker) lock(ctx context.Context, key, val string, ttl time.Duration) error {
 	ok, err := l.client.SetNX(ctx, key, val, ttl).Result()
-	if !ok || errors.Is(err, redis.Nil) {
+	if err != nil {
+		return fmt.Errorf("lock: %w", err)
+	}
+	if !ok {
 		return ErrLocked
 	}
 
@@ -129,25 +140,24 @@ func (l *Locker) unlock(ctx context.Context, key, val string) error {
 		return ErrLockReleased
 	}
 
-	return err
+	if err != nil {
+		return fmt.Errorf("unlock: %w", err)
+	}
+
+	return nil
 }
 
 func (l *Locker) extend(ctx context.Context, key, val string, ttl time.Duration) error {
 	keys := []string{key}
-	argv := []any{val, formatMs(ttl)}
+	argv := []any{val, ttl.Milliseconds()}
 	err := extend.Run(ctx, l.client, keys, argv...).Err()
 	if errors.Is(err, redis.Nil) {
 		return ErrLockReleased
 	}
 
-	return err
-}
-
-// copied from redis source code
-func formatMs(dur time.Duration) int64 {
-	if dur > 0 && dur < time.Millisecond {
-		return 1
+	if err != nil {
+		return fmt.Errorf("extend: %w", err)
 	}
 
-	return int64(dur / time.Millisecond)
+	return nil
 }
