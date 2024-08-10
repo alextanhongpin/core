22472005d49d413a4cef54bb97a1434dcff99078
alextanhongpin <alextan220990@gmail.com>
2024-08-10 15:25:28 +0800

    fix(sync/promise): failfast for All, cleanup methods

--- sync/promise/promise.go
+++ sync/promise/promise.go
@@ -1,7 +1,6 @@
 package promise
 
 import (
-	"errors"
 	"sync"
 )
 
@@ -19,20 +18,15 @@ func Deferred[T any]() *Promise[T] {
 }
 
 func Resolve[T any](v T) *Promise[T] {
-	p := &Promise[T]{}
-	p.wg.Add(1)
-	return p.Resolve(v)
+	return Deferred[T]().Resolve(v)
 }
 
 func Reject[T any](err error) *Promise[T] {
-	p := &Promise[T]{}
-	p.wg.Add(1)
-	return p.Reject(err)
+	return Deferred[T]().Reject(err)
 }
 
 func New[T any](fn func() (T, error)) *Promise[T] {
-	p := &Promise[T]{}
-	p.wg.Add(1)
+	p := Deferred[T]()
 
 	go func() {
 		v, err := fn()
@@ -79,17 +73,12 @@ type Promises[T any] []*Promise[T]
 func (promises Promises[T]) All() ([]T, error) {
 	res := make([]T, len(promises))
 
-	var errs error
 	for i, p := range promises {
 		v, err := p.Await()
 		if err != nil {
-			errs = errors.Join(errs, err)
-		} else {
-			res[i] = v
+			return nil, err
 		}
-	}
-	if errs != nil {
-		return nil, errs
+		res[i] = v
 	}
 
 	return res, nil
