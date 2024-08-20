a16ea373884191ff8b797a386de866cf821ef10f
alextanhongpin <alex.tan@bandlab.com>
2024-08-20 13:09:27 +0800

    feat(sync/batch): add queue

---
+++ sync/batch/queue.go
@@ -0,0 +1,108 @@
+package batch
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"maps"
+	"sync"
+
+	"github.com/alextanhongpin/core/sync/promise"
+)
+
+type Queue[K comparable, V any] struct {
+	loader loader[K, V]
+	group  *promise.Group[V]
+
+	mu   sync.Mutex
+	keys map[K]struct{}
+}
+
+func NewQueue[K comparable, V any](l loader[K, V]) *Queue[K, V] {
+	return &Queue[K, V]{
+		loader: l,
+		group:  promise.NewGroup[V](),
+		keys:   make(map[K]struct{}),
+	}
+}
+
+func (q *Queue[K, V]) AddMany(keys ...K) {
+	q.mu.Lock()
+	for _, key := range keys {
+		_, loaded := q.group.LoadOrStore(fmt.Sprint(key))
+		if loaded {
+			continue
+		}
+
+		q.keys[key] = struct{}{}
+	}
+	q.mu.Unlock()
+}
+
+func (q *Queue[K, V]) Add(key K) bool {
+	_, loaded := q.group.LoadOrStore(fmt.Sprint(key))
+	if loaded {
+		return false
+	}
+
+	q.mu.Lock()
+	q.keys[key] = struct{}{}
+	q.mu.Unlock()
+
+	return true
+}
+
+func (q *Queue[K, V]) Load(key K) (v V, err error) {
+	p, ok := q.group.Load(fmt.Sprint(key))
+	if !ok {
+		return v, newKeyError(fmt.Sprint(key), ErrKeyNotExist)
+	}
+
+	return p.Await()
+}
+
+func (q *Queue[K, V]) LoadMany(keys []K) (v []V, err error) {
+	vs := make([]V, 0, len(keys))
+	for _, key := range keys {
+		p, loaded := q.group.Load(fmt.Sprint(key))
+		if !loaded {
+			continue
+		}
+		v, err := p.Await()
+		if errors.Is(err, ErrKeyNotExist) {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+		vs = append(vs, v)
+	}
+
+	return vs, nil
+}
+
+func (q *Queue[K, V]) Flush(ctx context.Context) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	kv := maps.Clone(q.keys)
+	clear(q.keys)
+
+	keys := make([]K, 0, len(kv))
+	for k := range kv {
+		keys = append(keys, k)
+	}
+
+	m, err := q.loader.LoadManyResult(ctx, keys)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range m {
+		p, ok := q.group.Load(fmt.Sprint(k))
+		if ok {
+			p.Wait(v.Unwrap())
+		}
+	}
+
+	return nil
+}
---
+++ sync/batch/queue_test.go
@@ -0,0 +1,41 @@
+package batch_test
+
+import (
+	"testing"
+
+	"github.com/alextanhongpin/core/sync/batch"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueue_Load(t *testing.T) {
+	loader := newBatchLoader()
+	q := batch.NewQueue(loader)
+
+	q.Add(1)
+	q.Flush(ctx)
+
+	v, err := q.Load(1)
+	is := assert.New(t)
+	is.Nil(err)
+	is.Equal("1", v)
+
+	v, err = q.Load(-1)
+	is.ErrorIs(err, batch.ErrKeyNotExist)
+	is.Equal("", v)
+}
+
+func TestQueue_LoadMany(t *testing.T) {
+	loader := newBatchLoader()
+	q := batch.NewQueue(loader)
+	q.AddMany(1, 2, 3)
+	q.Flush(ctx)
+
+	vs, err := q.LoadMany([]int{1, 2, 3})
+	is := assert.New(t)
+	is.Nil(err)
+	is.Equal([]string{"1", "2", "3"}, vs)
+
+	vs, err = q.LoadMany([]int{-1, -2, -3})
+	is.Nil(err, err)
+	is.Empty(vs)
+}
