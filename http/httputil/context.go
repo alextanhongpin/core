250525a1c8baa3c1a8244a131afec97bd584cffe
alextanhongpin <alextan220990@gmail.com>
2024-09-08 21:51:27 +0800

    feat(http/httputil): add must validation

--- http/httputil/context.go
+++ http/httputil/context.go
@@ -2,7 +2,11 @@ package httputil
 
 import (
 	"context"
+	"errors"
+	"fmt"
 )
+
+var ErrContextKeyNotFound = errors.New("httputil: context key not found")
 
 type Context[T any] string
 
@@ -13,4 +17,13 @@ func (k Context[T]) WithValue(ctx context.Context, t T) context.Context {
 func (k Context[T]) Value(ctx context.Context) (T, bool) {
 	t, ok := ctx.Value(k).(T)
 	return t, ok
+}
+
+func (k Context[T]) MustValue(ctx context.Context) T {
+	t, ok := k.Value(ctx)
+	if !ok {
+		panic(fmt.Errorf("%w: %s", ErrContextKeyNotFound, k))
+	}
+
+	return t
 }
