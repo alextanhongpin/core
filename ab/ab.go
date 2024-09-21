61f9cf330f77fc33297101949de64e83f0055412
alextanhongpin <alextan220990@gmail.com>
2024-09-21 14:11:49 +0800

    feat(ab): add ab testing utility

---
+++ ab/ab.go
@@ -0,0 +1,11 @@
+package ab
+
+import "github.com/spaolacci/murmur3"
+
+func Hash(key string, size uint64) uint64 {
+	return murmur3.Sum64([]byte(key)) % size
+}
+
+func Rollout(key string, percentage uint64) bool {
+	return percentage > 0 && Hash(key, 100) <= percentage
+}
---
+++ ab/go.mod
@@ -0,0 +1,5 @@
+module github.com/alextanhongpin/core/ab
+
+go 1.23.0
+
+require github.com/spaolacci/murmur3 v1.1.0 // indirect
