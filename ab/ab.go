c4783f9b83e1f5476326bb918b774c51966a4362
alextanhongpin <alextan220990@gmail.com>
2024-10-05 16:41:04 +0800

    chore: add unique counter

--- ab/ab.go
+++ ab/ab.go
@@ -1,6 +1,11 @@
 package ab
 
-import "github.com/spaolacci/murmur3"
+import (
+	"context"
+
+	redis "github.com/redis/go-redis/v9"
+	"github.com/spaolacci/murmur3"
+)
 
 func Hash(key string, size uint64) uint64 {
 	return murmur3.Sum64([]byte(key)) % size
@@ -8,4 +13,17 @@ func Hash(key string, size uint64) uint64 {
 
 func Rollout(key string, percentage uint64) bool {
 	return percentage > 0 && Hash(key, 100) <= percentage
+}
+
+type Unique struct {
+	client *redis.Client
+}
+
+func (u *Unique) Store(ctx context.Context, key, val string) (bool, error) {
+	n, err := u.client.PFAdd(ctx, key, val).Result()
+	return n == 1, err
+}
+
+func (u *Unique) Load(ctx context.Context, key string) (card int64, err error) {
+	return u.client.PFCount(ctx, key).Result()
 }
--- ab/go.mod
+++ ab/go.mod
@@ -2,4 +2,12 @@ module github.com/alextanhongpin/core/ab
 
 go 1.23.0
 
-require github.com/spaolacci/murmur3 v1.1.0 // indirect
+require (
+	github.com/redis/go-redis/v9 v9.6.1
+	github.com/spaolacci/murmur3 v1.1.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
