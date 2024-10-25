f28951b7a9e79ee27e8fe078830e6e42cb692cdd
alextanhongpin <alex.tan@bandlab.com>
2024-10-25 09:57:35 +0800

    chore: update tests

--- types/htmplates/htmplates_test.go
+++ types/htmplates/htmplates_test.go
@@ -11,12 +11,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//go:embed testdata/*.html
+//go:embed testdata/*.html testdata/**/*.html
 var templates embed.FS
 
 func TestRender(t *testing.T) {
 	t.Run("no hot-reload", func(t *testing.T) {
-		engine := htmplates.Engine{
+		html5 := htmplates.Engine{
 			BaseDir: "testdata",
 			FS:      templates,
 			Funcs: template.FuncMap{
@@ -26,23 +26,67 @@ func TestRender(t *testing.T) {
 			},
 		}
 
-		tpl := engine.Compile("function.html")()
+		tf := html5.Compile("function.html")
 
 		var b bytes.Buffer
-		err := tpl.Execute(&b, nil)
+		err := tf().Execute(&b, nil)
 
 		is := assert.New(t)
 		is.Nil(err)
 		is.Equal("Hello, world!\n", b.String())
+
+		html5.Funcs = template.FuncMap{
+			"Greet": func(msg string) string {
+				return "hello world"
+			},
+		}
+
+		b.Reset()
+		err = tf().Execute(&b, nil)
+		is.Nil(err)
+		is.Equal("Hello, world!\n", b.String())
+	})
+
+	t.Run("hot-reload", func(t *testing.T) {
+		html5 := htmplates.Engine{
+			BaseDir:   "testdata",
+			FS:        templates,
+			HotReload: true,
+			Funcs: template.FuncMap{
+				"Greet": func(msg string) string {
+					return fmt.Sprintf("Hello, %s!", msg)
+				},
+			},
+		}
+
+		tf := html5.Compile("function.html")
+
+		var b bytes.Buffer
+		err := tf().Execute(&b, nil)
+
+		is := assert.New(t)
+		is.Nil(err)
+		is.Equal("Hello, world!\n", b.String())
+
+		html5.Funcs = template.FuncMap{
+			"Greet": func(msg string) string {
+				return "hello world"
+			},
+		}
+
+		b.Reset()
+		err = tf().Execute(&b, nil)
+		is.Nil(err)
+		is.Equal("hello world\n", b.String())
 	})
 
 	t.Run("subtemplate", func(t *testing.T) {
-		engine := htmplates.Engine{
+		html5 := htmplates.Engine{
 			BaseDir: "testdata",
 			FS:      templates,
 		}
 
-		tpl := engine.Compile("index.html", "extra.html")()
+		tpl := html5.Compile("index.html", "extra.html")()
 
 		var b bytes.Buffer
 		err := tpl.Execute(&b, nil)
@@ -50,5 +94,21 @@ func TestRender(t *testing.T) {
 		is := assert.New(t)
 		is.Nil(err)
 		is.Equal("Hello, \nworld\n\n", b.String())
+	})
+
+	t.Run("subtemplates", func(t *testing.T) {
+		html5 := htmplates.Engine{
+			BaseDir: "testdata",
+			FS:      templates,
+		}
+
+		tpl := html5.Compile("base.html", "components/*.html")()
+
+		var b bytes.Buffer
+		err := tpl.Execute(&b, nil)
+
+		is := assert.New(t)
+		is.Nil(err)
+		is.Equal("\nheader\n\n\nfooter\n\n", b.String())
 	})
 }
