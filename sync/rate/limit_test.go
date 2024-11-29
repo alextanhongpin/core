23f462749e6f43e47554b1e24fae0012243936c2
alextanhongpin <alex.tan@bandlab.com>
2024-11-29 13:42:15 +0800

    chore: add test

--- sync/rate/limit_test.go
+++ sync/rate/limit_test.go
@@ -2,11 +2,167 @@ package rate_test
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"testing"
+	"text/tabwriter"
+
+	"math/rand/v2"
 
 	"github.com/alextanhongpin/core/sync/rate"
 	"github.com/stretchr/testify/assert"
 )
+
+func ExampleLimiter() {
+	r1 := rate.NewLimiter(10)
+	r1.SuccessToken = 0.9
+
+	r2 := rate.NewLimiter(10)
+	r2.SuccessToken = 0.5
+
+	r3 := rate.NewLimiter(10)
+	r3.SuccessToken = 0.1
+
+	var failure int
+	allows := make([]int, 3)
+	r := rand.New(rand.NewPCG(1, 2))
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+
+	fmt.Fprintf(w, "%s\t%s\t%s\t\n", "0.9", "0.5", "0.1")
+	for range 100 {
+		a1, a2, a3 := r1.Allow(), r2.Allow(), r3.Allow()
+		if a1 {
+			allows[0]++
+		}
+		if a2 {
+			allows[1]++
+		}
+		if a3 {
+			allows[2]++
+		}
+
+		fmt.Fprintf(w, "%t\t%t\t%t\t\n", a1, a2, a3)
+		if r.Float64() < 0.8 {
+			failure++
+			r1.Err()
+			r2.Err()
+			r3.Err()
+		} else {
+			r1.Ok()
+			r2.Ok()
+			r3.Ok()
+		}
+	}
+	fmt.Fprintf(w, "%d\t%d\t%d\t\n", allows[0], allows[1], allows[2])
+	fmt.Println("failure:", failure)
+	fmt.Println("success token:")
+	w.Flush()
+	// Output:
+	// failure: 68
+	// success token:
+	//    0.9|   0.5|   0.1|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true| false|
+	//   true| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true|  true| false|
+	//   true|  true|  true|
+	//   true|  true| false|
+	//   true|  true|  true|
+	//   true|  true| false|
+	//   true| false| false|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//   true|  true|  true|
+	//   true| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//  false| false| false|
+	//  false| false| false|
+	//   true|  true|  true|
+	//   true|  true|  true|
+	//     59|    46|    42|
+}
 
 func TestLimit(t *testing.T) {
 	badRequestErr := errors.New("bad request")
