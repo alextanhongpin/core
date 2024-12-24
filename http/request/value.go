32ae3fd135ca388e2cd19db5822ce30c57e5b9ff
alextanhongpin <alex.tan@bandlab.com>
2024-12-24 19:51:11 +0800

    chore(http/request): add Value

---
+++ http/request/value.go
@@ -0,0 +1,85 @@
+package request
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"reflect"
+	"strconv"
+)
+
+type Value string
+
+func QueryValue(r *http.Request, name string) Value {
+	return Value(r.URL.Query().Get(name))
+}
+
+func PathValue(r *http.Request, name string) Value {
+	return Value(r.PathValue(name))
+}
+
+func (v Value) String() string {
+	return string(v)
+}
+
+func (v Value) Int64() int64 {
+	return toInt64(v.String())
+}
+
+func (v Value) Int32() int32 {
+	return toInt32(v.String())
+}
+
+func (v Value) Int() int {
+	return toInt(v.String())
+}
+
+func (v Value) Bool() bool {
+	return toBool(v.String())
+}
+
+func (v Value) FromBase64() Value {
+	return Value(fromBase64(v.String()))
+}
+
+func (v Value) ToBase64() Value {
+	return Value(toBase64(v.String()))
+}
+
+func toInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func toInt32(s string) int32 {
+	i, _ := strconv.ParseInt(s, 10, 32)
+	return int32(i)
+}
+
+func toInt64(s string) int64 {
+	i, _ := strconv.ParseInt(s, 10, 64)
+	return i
+}
+
+func toBool(s string) bool {
+	b, _ := strconv.ParseBool(s)
+	return b
+}
+
+func fromBase64(s string) string {
+	b, _ := base64.URLEncoding.DecodeString(s)
+	return string(b)
+}
+
+func toBase64(s any) string {
+	// Skip zero values. We don't want the user to accidentally use the cursor.
+	if isZero(s) {
+		return ""
+	}
+
+	return base64.URLEncoding.EncodeToString([]byte(fmt.Sprint(s)))
+}
+
+func isZero(a any) bool {
+	return reflect.ValueOf(a).IsZero()
+}
