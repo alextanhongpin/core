9778596ba2bcf7989adc69c13b41bef618702792
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-04-05 17:28:56 +0800

    fix(types): simplify useragent

--- types/useragent/useragent.go
+++ types/useragent/useragent.go
@@ -15,6 +15,48 @@ import (
 
 var Browsers = []string{"Chrome", "Firefox", "Mozilla", "Safari", "Opera", "Opera Mini", "Edge", "Internet Explorer"}
 
+func For(browser string) (iter.Seq[string], error) {
+	baseURL := "http://www.useragentstring.com/pages/useragentstring.php?name="
+	resp, err := http.Get(fmt.Sprintf("%s%s", baseURL, browser))
+	if err != nil {
+		return nil, fmt.Errorf("http.Get error: %w", err)
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("html.Parse error: %w", err)
+	}
+
+	seq := func(yield func(string) bool) {
+		for n := range doc.Descendants() {
+			if n.Type == html.ElementNode && n.DataAtom == atom.Div {
+				for _, attr := range n.Attr {
+					if attr.Key == "id" && attr.Val == "liste" {
+						for nn := range n.Descendants() {
+							if nn.Type == html.ElementNode && nn.DataAtom == atom.A {
+								text := nn.FirstChild.Data
+								if slices.Contains([]string{"Google", "Chrome"}, text) {
+									continue
+								}
+								if !yield(text) {
+									break
+								}
+							}
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return seq, nil
+}
+
 type loader interface {
 	io.ReaderFrom
 	io.WriterTo
@@ -56,7 +98,10 @@ func (l *Loader) ReadFrom(r io.Reader) (int64, error) {
 func (l *Loader) WriteTo(w io.Writer) (int64, error) {
 	var total int
 	for _, b := range l.browsers {
-		seq, stop := Fetch(b)
+		seq, err := For(b)
+		if err != nil {
+			return 0, err
+		}
 		for ua := range seq {
 			n, err := fmt.Fprintf(w, "%s\n", ua)
 			if err != nil {
@@ -64,73 +109,7 @@ func (l *Loader) WriteTo(w io.Writer) (int64, error) {
 			}
 			total += n
 		}
-		if err := stop(); err != nil {
-			return 0, err
-		}
 	}
 
 	return int64(total), nil
-}
-
-func Fetch(browser string) (iter.Seq[string], func() error) {
-	baseURL := "http://www.useragentstring.com/pages/useragentstring.php?name="
-	var iterErr error
-	seq := func(yield func(string) bool) {
-		resp, err := http.Get(fmt.Sprintf("%s%s", baseURL, browser))
-		if err != nil {
-			iterErr = err
-			return
-		}
-		defer func() {
-			_ = resp.Body.Close()
-		}()
-
-		iterUA, err := parseUserAgentFromHTML(resp.Body)
-		if err != nil {
-			iterErr = err
-			return
-		}
-
-		for ua := range iterUA {
-			if !yield(ua) {
-				break
-			}
-		}
-	}
-
-	return seq, func() error {
-		return iterErr
-	}
-}
-
-func parseUserAgentFromHTML(r io.Reader) (iter.Seq[string], error) {
-	doc, err := html.Parse(r)
-	if err != nil {
-		return nil, err
-	}
-
-	seq := func(yield func(string) bool) {
-		for n := range doc.Descendants() {
-			if n.Type == html.ElementNode && n.DataAtom == atom.Div {
-				for _, attr := range n.Attr {
-					if attr.Key == "id" && attr.Val == "liste" {
-						for nn := range n.Descendants() {
-							if nn.Type == html.ElementNode && nn.DataAtom == atom.A {
-								text := nn.FirstChild.Data
-								if slices.Contains([]string{"Google", "Chrome"}, text) {
-									continue
-								}
-								if !yield(text) {
-									break
-								}
-							}
-						}
-						break
-					}
-				}
-			}
-		}
-	}
-
-	return seq, nil
 }
