75495cd2eda2cc760f39cbc528e160c8b1c51139
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-05-04 13:16:40 +0800

    refactor(types/tracker): simplify lifecycle

--- types/tracker/tracker.go
+++ types/tracker/tracker.go
@@ -1,9 +1,11 @@
 package tracker
 
 import (
+	"cmp"
 	"context"
 	"fmt"
 	"log/slog"
+	"sync"
 	"time"
 )
 
@@ -11,14 +13,9 @@ import (
 // It collects contextual attributes, tracks an error if one occurs, and logs
 // a final summary message upon calling Done().
 type Tracker struct {
-	ctx context.Context
-	// attrs collects all attributes (key/value pairs) associated with this tracked operation.
-	attrs []slog.Attr
-	// err stores any error encountered during the tracked operation.
-	err error
-	// msg holds the primary message to be logged when Done() is called.
-	msg string
 	// start records the time the tracking process began.
+	*slog.Logger
+	once  sync.Once
 	start time.Time
 }
 
@@ -28,69 +25,45 @@ type Tracker struct {
 // ctx: The context to be used for all logging operations within this tracker.
 // msg: The primary message to be logged when the operation completes successfully.
 // attrs: Any initial attributes to be associated with this operation.
-func New(ctx context.Context, msg string, attrs ...slog.Attr) *Tracker {
-	t := &Tracker{
-		ctx:   ctx,
-		attrs: attrs,
-		msg:   msg,
-		start: time.Now(),
+func New(logger *slog.Logger) *Tracker {
+	return &Tracker{
+		Logger: cmp.Or(logger, slog.Default()),
+		start:  time.Now(),
 	}
-	return t.Debug(msg)
 }
 
 // Error sets the tracker's internal error state.
 // If this method is called, the final log recorded by Done() will treat the operation as failed.
-func (t *Tracker) Error(err error) error {
-	t.err = err
-	return t.err
+func (t *Tracker) Error(ctx context.Context, err error) error {
+	t.done(ctx, "", err)
+	return err
 }
 
 // Errorf sets the tracker's internal error state using formatted error messages.
 // If this method is called, the final log recorded by Done() will treat the operation as failed.
-func (t *Tracker) Errorf(format string, args ...any) error {
-	return t.Error(fmt.Errorf(format, args...))
+func (t *Tracker) Errorf(ctx context.Context, format string, args ...any) error {
+	return t.Error(ctx, fmt.Errorf(format, args...))
+}
+
+func (t *Tracker) With(args ...any) *Tracker {
+	t.Logger = t.Logger.With(args...)
+	return t
 }
 
 // Attrs appends a variable list of attributes to the tracker's context.
 // Use this to attach metadata that should be part of the final log record.
 // It returns the tracker pointer for chaining.
 func (t *Tracker) Attrs(attrs ...slog.Attr) *Tracker {
-	t.attrs = append(t.attrs, attrs...)
-	return t
+	a := make([]any, len(attrs))
+	for i, attr := range attrs {
+		a[i] = attr
+	}
+
+	return t.With(a...)
 }
 
-// Info logs an informational message to the tracker's context.
-// This is useful for logging interim steps *before* calling Done().
-// It does not change the error state.
-func (t *Tracker) Info(msg string, attrs ...slog.Attr) *Tracker {
-	slog.LogAttrs(t.ctx, slog.LevelInfo, msg, append(t.attrs, attrs...)...)
-	return t
-}
-
-// Infof logs an informational message using a format string.
-// This is functionally similar to Info but kept for potential semantic difference
-// if logging levels become more granular later.
-// It does not change the error state.
-func (t *Tracker) Infof(format string, args ...any) *Tracker {
-	slog.LogAttrs(t.ctx, slog.LevelInfo, fmt.Sprintf(format, args...), t.attrs...)
-	return t
-}
-
-// Debug logs a debug message to the tracker's context.
-// This is useful for logging interim steps *before* calling Done().
-// It does not change the error state.
-func (t *Tracker) Debug(msg string, attrs ...slog.Attr) *Tracker {
-	slog.LogAttrs(t.ctx, slog.LevelDebug, msg, append(t.attrs, attrs...)...)
-	return t
-}
-
-// Debugf logs a debug message using a format string.
-// This is functionally similar to Info but kept for potential semantic difference
-// if logging levels become more granular later.
-// It does not change the error state.
-func (t *Tracker) Debugf(format string, args ...any) *Tracker {
-	slog.LogAttrs(t.ctx, slog.LevelDebug, fmt.Sprintf(format, args...), t.attrs...)
-	return t
+func (t *Tracker) Done(ctx context.Context, msg string) {
+	t.done(ctx, msg, nil)
 }
 
 // Done finalizes the tracking process.
@@ -98,20 +71,17 @@ func (t *Tracker) Debugf(format string, args ...any) *Tracker {
 // and returns the tracker pointer for chaining.
 // If t.err is non-nil, the log level will be ERROR.
 // Otherwise, it logs at INFO level.
-func (t *Tracker) Done() {
-	// Calculate and append the total duration as the final attribute.
-	duration := time.Since(t.start)
-	finalAttrs := append(t.attrs, slog.Duration("took", duration))
+func (t *Tracker) done(ctx context.Context, msg string, err error) {
+	t.once.Do(func() {
+		// Calculate and append the total duration as the final attribute.
+		tookAttr := slog.Duration("took", time.Since(t.start))
 
-	if t.err != nil {
-		// Log an error summary
-		finalAttrs = append(finalAttrs, slog.String("cause", t.err.Error()))
-		slog.LogAttrs(t.ctx, slog.LevelError, t.msg, finalAttrs...)
-	} else {
-		// Log a success summary
-		slog.LogAttrs(t.ctx, slog.LevelInfo, t.msg, finalAttrs...)
-	}
-
-	// Resetting the error state might be desirable if the tracker is reused,
-	// but for simplicity, we leave it as is, assuming Done() is the end of a cycle.
+		if err != nil {
+			// Log an error summary
+			t.Logger.LogAttrs(ctx, slog.LevelError, msg, tookAttr, slog.String("cause", err.Error()))
+		} else {
+			// Log a success summary
+			t.Logger.LogAttrs(ctx, slog.LevelInfo, msg, tookAttr)
+		}
+	})
 }
--- types/tracker/tracker_test.go
+++ types/tracker/tracker_test.go
@@ -2,10 +2,12 @@ package tracker_test
 
 import (
 	"bytes"
+	"cmp"
 	"context"
 	"errors"
 	"fmt"
 	"log/slog"
+	"slices"
 	"time"
 
 	"github.com/alextanhongpin/core/types/tracker"
@@ -15,7 +17,7 @@ var bb = new(bytes.Buffer)
 
 func init() {
 	// 1. Initialize with a base handler (e.g., Text)
-	baseHandler := slog.NewTextHandler(bb, &slog.HandlerOptions{
+	next := slog.NewTextHandler(bb, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if a.Key == "took" {
@@ -27,58 +29,98 @@ func init() {
 			return a
 		},
 	})
-	logger := slog.New(&ReqIDHandler{Handler: baseHandler})
+	logger := slog.New(&ReqIDHandler{Next: next})
 	slog.SetDefault(logger)
 }
 
 func ExampleNew_error() {
 	bb.Reset()
-	// 2. Add Request ID to context (usually done in HTTP middleware)
-	ctx := context.WithValue(context.Background(), requestIDKey, "req-123abc")
 
-	_ = foo(ctx, errors.ErrUnsupported)
+	_ = foo(context.Background(), errors.ErrUnsupported, slog.Default())
 	fmt.Println(bb.String())
 
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=foo key=value request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=retry call=api request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=0 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=1 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=2 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retry call=api took=0s request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg=foo key=value took=0s cause="unsupported operation" request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=foo key=value
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=retry api=bar
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done op=retry api=bar took=0s
+	// time=0001-01-01T00:00:00.000Z level=ERROR msg="" op=foo key=value took=0s cause="unsupported operation"
 }
 
-func ExampleNew_info() {
+func ExampleNew_success() {
+	bb.Reset()
+
+	_ = foo(context.Background(), nil, nil)
+	fmt.Println(bb.String())
+	// Output:
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=foo key=value
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=retry api=bar
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done op=retry api=bar took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done op=foo key=value took=0s
+}
+
+func ExampleNew_requestID() {
 	bb.Reset()
 
 	// 2. Add Request ID to context (usually done in HTTP middleware)
 	ctx := context.WithValue(context.Background(), requestIDKey, "req-123abc")
 
-	_ = foo(ctx, nil)
+	_ = foo(ctx, nil, nil)
 	fmt.Println(bb.String())
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=foo key=value request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=retry call=api request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=0 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=1 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... call=api attempts=2 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retry call=api took=0s request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=foo key=value took=0s request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=foo key=value request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init op=retry api=bar request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=0 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=1 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... op=retry api=bar attempts=2 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done op=retry api=bar took=0s request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done op=foo key=value took=0s request_id=req-123abc
 }
 
-func foo(ctx context.Context, err error) error {
-	t := tracker.New(ctx, "foo", slog.String("key", "value"))
-	defer t.Done()
+func ExampleNew_discard() {
+	bb.Reset()
 
-	tt := tracker.New(ctx, "retry", slog.String("call", "api"))
+	_ = foo(context.Background(), nil, slog.New(slog.DiscardHandler))
+	fmt.Println(bb.String())
+	// Output:
+	//
+}
+
+func ExampleNew_group() {
+	bb.Reset()
+
+	_ = foo(context.Background(), nil, slog.Default().WithGroup("group"))
+	fmt.Println(bb.String())
+	// Output:
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init group.op=foo group.key=value
+	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init group.op=retry group.api=bar
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.op=retry group.api=bar group.attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.op=retry group.api=bar group.attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.op=retry group.api=bar group.attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done group.op=retry group.api=bar group.took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO msg=done group.op=foo group.key=value group.took=0s
+}
+
+func foo(ctx context.Context, err error, logger *slog.Logger) error {
+	logger = cmp.Or(logger, slog.Default())
+	t := tracker.New(logger).With("op", "foo", slog.String("key", "value"))
+	t.DebugContext(ctx, "init")
+	defer t.Done(ctx, "done")
+
+	tt := tracker.New(logger).Attrs(slog.String("op", "retry"), slog.String("api", "bar"))
+	tt.DebugContext(ctx, "init")
 	for i := range 3 {
-		tt.Info("retrying...", slog.Int("attempts", i))
+		tt.InfoContext(ctx, "retrying...", slog.Int("attempts", i))
 	}
-	tt.Done()
+	tt.Done(ctx, "done")
 
 	if err != nil {
-		return t.Error(err)
+		return t.Error(ctx, err)
 	}
 
 	return nil
@@ -90,7 +132,9 @@ const requestIDKey contextKey = "request_id"
 
 // ReqIDHandler wraps an existing handler to inject context values
 type ReqIDHandler struct {
-	slog.Handler
+	Next  slog.Handler
+	group string
+	attrs []slog.Attr
 }
 
 func (h *ReqIDHandler) Handle(ctx context.Context, r slog.Record) error {
@@ -98,5 +142,35 @@ func (h *ReqIDHandler) Handle(ctx context.Context, r slog.Record) error {
 	if id, ok := ctx.Value(requestIDKey).(string); ok {
 		r.AddAttrs(slog.String("request_id", id))
 	}
-	return h.Handler.Handle(ctx, r)
+	next := h.Next
+	if h.group != "" {
+		next = next.WithGroup(h.group)
+	}
+	return next.WithAttrs(h.attrs).Handle(ctx, r)
+}
+
+func (h *ReqIDHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	// NOTE: Returning h.Next.WithAttrs(attrs) will result in h.Handle using the
+	// original h.Next.Handle.
+	c := h.clone()
+	c.attrs = append(h.attrs, attrs...)
+	return c
+}
+
+func (h *ReqIDHandler) WithGroup(name string) slog.Handler {
+	c := h.clone()
+	c.group = name
+	return c
+}
+
+func (h *ReqIDHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.Next.Enabled(ctx, level)
+}
+
+func (h *ReqIDHandler) clone() *ReqIDHandler {
+	return &ReqIDHandler{
+		Next:  h.Next,
+		group: h.group,
+		attrs: slices.Clone(h.attrs),
+	}
 }
