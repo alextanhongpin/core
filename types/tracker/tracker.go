859297f47d44fa9b97173e21f90786b7a19564e5
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-06-23 12:22:59 +0800

    chore(types/tracker): fix source

--- types/tracker/tracker.go
+++ types/tracker/tracker.go
@@ -1,10 +1,10 @@
 package tracker
 
 import (
-	"cmp"
 	"context"
 	"fmt"
 	"log/slog"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -19,62 +19,87 @@ type Tracker struct {
 	start  time.Time
 }
 
+func NewWithLogger(ctx context.Context, logger *slog.Logger, attrs ...slog.Attr) *Tracker {
+	t := &Tracker{
+		ctx:    ctx,
+		logger: logger,
+		start:  time.Now(),
+	}
+	t.SetAttrs(attrs...)
+	return t
+}
+
 // New creates and returns a new Tracker instance.
 // It initializes the timer and sets the initial message context.
 //
 // ctx: The context to be used for all logging operations within this tracker.
 // msg: The primary message to be logged when the operation completes successfully.
 // attrs: Any initial attributes to be associated with this operation.
-func New(ctx context.Context, logger *slog.Logger, attrs ...slog.Attr) *Tracker {
+func New(ctx context.Context, attrs ...slog.Attr) *Tracker {
 	t := &Tracker{
 		ctx:    ctx,
-		logger: cmp.Or(logger, slog.Default()),
+		logger: slog.Default(),
 		start:  time.Now(),
 	}
-	return t.WithAttrs(attrs...)
+	t.SetAttrs(attrs...)
+	return t
 }
 
-func (t *Tracker) Debug(msg string, attrs ...slog.Attr) *Tracker {
-	t.logger.LogAttrs(t.ctx, slog.LevelDebug, msg, attrs...)
-	return t
+func (t *Tracker) Debug(msg string, attrs ...slog.Attr) {
+	t.Log(t.ctx, 3, slog.LevelDebug, msg, attrs...)
 }
 
 func (t *Tracker) Error(err error, attrs ...slog.Attr) error {
 	t.once.Do(func() {
 	})
-	t.logger.LogAttrs(t.ctx, slog.LevelError, err.Error(), append(attrs, slog.Duration("took", time.Since(t.start)))...)
+	t.Log(t.ctx, 3, slog.LevelError, err.Error(), slog.Duration("took", time.Since(t.start)))
 	return err
 }
 
 func (t *Tracker) Errorf(format string, args ...any) error {
-	return t.Error(fmt.Errorf(format, args...))
+	err := fmt.Errorf(format, args...)
+
+	t.once.Do(func() {
+	})
+	t.Log(t.ctx, 3, slog.LevelError, err.Error(), slog.Duration("took", time.Since(t.start)))
+	return err
 }
 
-func (t *Tracker) Info(msg string, attrs ...slog.Attr) *Tracker {
-	t.logger.LogAttrs(t.ctx, slog.LevelInfo, msg, attrs...)
-	return t
+func (t *Tracker) Info(msg string, attrs ...slog.Attr) {
+	t.Log(t.ctx, 3, slog.LevelInfo, msg, attrs...)
 }
 
-func (t *Tracker) Warn(msg string, attrs ...slog.Attr) *Tracker {
-	t.logger.LogAttrs(t.ctx, slog.LevelWarn, msg, attrs...)
-	return t
+func (t *Tracker) Warn(msg string, attrs ...slog.Attr) {
+	t.Log(t.ctx, 3, slog.LevelWarn, msg, attrs...)
 }
 
-// WithAttrs appends a variable list of attributes to the tracker's context.
-// Use this to attach metadata that should be part of the final log record.
-// It returns the tracker pointer for chaining.
-func (t *Tracker) WithAttrs(attrs ...slog.Attr) *Tracker {
+func (t *Tracker) Log(ctx context.Context, depth int, level slog.Level, msg string, attrs ...slog.Attr) {
+	var pc uintptr
+	var pcs [1]uintptr
+	runtime.Callers(depth, pcs[:])
+	pc = pcs[0]
+
+	// Create a new Record
+	// Signature: NewRecord(time time.Time, level Level, msg string, pc uintptr)
+	record := slog.NewRecord(time.Now(), level, msg, pc)
+	record.AddAttrs(attrs...)
+	_ = t.logger.Handler().Handle(ctx, record)
+}
+
+// SetAttrs appends a variable list of attributes to the tracker's context.
+// Use this to attach metadata that should be part of the final Log record.
+// We do not use "With" to avoid passing sync.Once.
+func (t *Tracker) SetAttrs(attrs ...slog.Attr) {
 	args := make([]any, len(attrs))
 	for i, attr := range attrs {
 		args[i] = attr
 	}
 	t.logger = t.logger.With(args...)
-	return t
 }
 
 // Done logs the message with duration.
 func (t *Tracker) Done(msg string, attrs ...slog.Attr) {
 	t.once.Do(func() {
-		t.logger.LogAttrs(t.ctx, slog.LevelInfo, msg, append(attrs, slog.Duration("took", time.Since(t.start)))...)
+		t.Log(t.ctx, 6, slog.LevelInfo, msg, append(attrs, slog.Duration("took", time.Since(t.start)))...)
 	})
 }
--- types/tracker/tracker_test.go
+++ types/tracker/tracker_test.go
@@ -18,7 +18,8 @@ var bb = new(bytes.Buffer)
 func init() {
 	// 1. Initialize with a base handler (e.g., Text)
 	next := slog.NewTextHandler(bb, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		AddSource: true,
+		Level:     slog.LevelDebug,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if a.Key == "took" {
 				a.Value = slog.DurationValue(0)
@@ -40,12 +41,12 @@ func ExampleNew_error() {
 	fmt.Println(bb.String())
 
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=foo key=value
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=0
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=1
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=2
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done api=bar took=0s
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg="unsupported operation" key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:138 msg=foo key=value
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:145 msg=done api=bar took=0s
+	// time=0001-01-01T00:00:00.000Z level=ERROR source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:148 msg="unsupported operation" key=value took=0s
 }
 
 func ExampleNew_success() {
@@ -54,12 +55,12 @@ func ExampleNew_success() {
 	_ = foo(context.Background(), nil, nil)
 	fmt.Println(bb.String())
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=foo key=value
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=0
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=1
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=2
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done api=bar took=0s
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:138 msg=foo key=value
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:145 msg=done api=bar took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:151 msg=done key=value took=0s
 }
 
 func ExampleNew_requestID() {
@@ -71,12 +72,12 @@ func ExampleNew_requestID() {
 	_ = foo(ctx, nil, nil)
 	fmt.Println(bb.String())
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=foo key=value request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=0 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=1 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... api=bar attempts=2 request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done api=bar took=0s request_id=req-123abc
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done key=value took=0s request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:138 msg=foo key=value request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=0 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=1 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... api=bar attempts=2 request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:145 msg=done api=bar took=0s request_id=req-123abc
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:151 msg=done key=value took=0s request_id=req-123abc
 }
 
 func ExampleNew_discard() {
@@ -94,46 +95,50 @@ func ExampleNew_group() {
 	_ = foo(context.Background(), nil, slog.Default().WithGroup("group"))
 	fmt.Println(bb.String())
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=foo group.key=value
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.api=bar group.attempts=0
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.api=bar group.attempts=1
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=retrying... group.api=bar group.attempts=2
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done group.api=bar group.took=0s
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done group.key=value group.took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:138 msg=foo group.key=value
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... group.api=bar group.attempts=0
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... group.api=bar group.attempts=1
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:143 msg=retrying... group.api=bar group.attempts=2
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:145 msg=done group.api=bar group.took=0s
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:151 msg=done group.key=value group.took=0s
 }
 
 func ExampleNew_idempotent() {
 	bb.Reset()
 
 	ctx := context.Background()
-	t := tracker.New(ctx, nil, slog.String("key", "value")).Debug("init")
+	t := tracker.New(ctx, slog.String("key", "value"))
+	t.Debug("init")
 	for range 3 {
 		t.Done("done")
 	}
 	_ = t.Errorf("bad request")
 
-	tt := tracker.New(ctx, nil, slog.String("key", "value")).Debug("retrying")
+	tt := tracker.New(ctx)
+	tt.SetAttrs(slog.String("key", "value"))
+	tt.Debug("retrying")
 	for i := range 3 {
 		_ = tt.Error(errors.New("retry error"), slog.Int("attempt", i+1))
 	}
 	tt.Done("retry ok")
 	fmt.Println(bb.String())
 	// Output:
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=init key=value
-	// time=0001-01-01T00:00:00.000Z level=INFO msg=done key=value took=0s
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg="bad request" key=value took=0s
-	// time=0001-01-01T00:00:00.000Z level=DEBUG msg=retrying key=value
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg="retry error" key=value attempt=1 took=0s
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg="retry error" key=value attempt=2 took=0s
-	// time=0001-01-01T00:00:00.000Z level=ERROR msg="retry error" key=value attempt=3 took=0s
+	// time=0001-01-01T00:00:00.000Z level=DEBUG source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:111 msg=init key=value
+	// time=0001-01-01T00:00:00.000Z level=INFO source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:113 msg=done key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=ERROR source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:115 msg="bad request" key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=DEBUG source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:119 msg=retrying key=value
+	// time=0001-01-01T00:00:00.000Z level=ERROR source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:121 msg="retry error" key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=ERROR source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:121 msg="retry error" key=value took=0s
+	// time=0001-01-01T00:00:00.000Z level=ERROR source=/Users/alextanhongpin/Documents/go/core/types/tracker/tracker_test.go:121 msg="retry error" key=value took=0s
 }
 
 func foo(ctx context.Context, err error, logger *slog.Logger) error {
 	logger = cmp.Or(logger, slog.Default())
-	t := tracker.New(ctx, logger, slog.String("key", "value")).Info("foo")
+	t := tracker.NewWithLogger(ctx, logger, slog.String("key", "value"))
+	t.Info("foo")
 	defer t.Done("done")
 
-	tt := tracker.New(ctx, logger, slog.String("api", "bar"))
+	tt := tracker.NewWithLogger(ctx, logger, slog.String("api", "bar"))
 	for i := range 3 {
 		tt.Info("retrying...", slog.Int("attempts", i))
 	}
