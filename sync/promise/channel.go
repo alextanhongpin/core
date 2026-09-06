a46c5c4df69bad1c9295f603c693d52cc91fe68b
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-09-06 11:54:19 +0800

    sync/promise: simplify sending channels

--- sync/promise/channel.go
+++ sync/promise/channel.go
@@ -2,9 +2,11 @@ package promise
 
 import (
 	"context"
-	"sync"
+	"errors"
 	"sync/atomic"
 )
+
+var errDone = errors.New("done")
 
 // Channel[T] is a generic single-value channel that delivers exactly one value
 // or terminates with a context cancellation error. It guarantees at most one
@@ -12,62 +14,54 @@ import (
 // is bound to a context; if the context is cancelled before a value is sent,
 // Recv returns context.Cause(ctx).
 type Channel[T any] struct {
-	ch     chan T
-	read   func() (T, error)
-	write  sync.Once
-	done   *atomic.Bool
 	cancel func(err error)
+	p      atomic.Pointer[T]
+	zero   *T
+	ctx    context.Context
 }
 
 // NewChannel creates a new Channel[T] bound to ctx. The returned channel will
 // return context.Cause(ctx) if the context is cancelled before a value is sent.
 func NewChannel[T any](ctx context.Context) *Channel[T] {
-	ch := make(chan T, 1)
-	done := new(atomic.Bool)
 	ctx, cancel := context.WithCancelCause(ctx)
-	return &Channel[T]{
-		done:   done,
-		ch:     ch,
+	ch := &Channel[T]{
+		ctx:    ctx,
 		cancel: cancel,
-		read: sync.OnceValues(func() (T, error) {
-			defer done.Store(true)
-
-			var zero T
-			select {
-			case <-ctx.Done():
-				return zero, context.Cause(ctx)
-
-			case res := <-ch:
-				return res, nil
-			}
-		}),
 	}
+	ch.zero = ch.p.Load()
+	return ch
 }
 
 // Send publishes v to the channel. It may be called only once; subsequent
 // calls are no-ops due to sync.Once.
 func (c *Channel[T]) Send(v T) {
-	c.write.Do(func() {
-		c.ch <- v
-	})
+	if c.p.CompareAndSwap(c.zero, &v) {
+		c.cancel(errDone)
+	}
 }
 
 // Recv blocks until a value is sent or the context is cancelled, then returns
 // the value and any error. The operation is executed at most once.
 func (c *Channel[T]) Recv() (T, error) {
-	return c.read()
+	<-c.ctx.Done()
+
+	err := context.Cause(c.ctx)
+	if !errors.Is(err, errDone) {
+		var zero T
+		return zero, err
+	}
+
+	return *c.p.Load(), nil
 }
 
 // Close cancels the underlying context with cause, preventing further sends
 // and causing Recv to return the cause error if no value was sent. It may be
 // called only once.
 func (c *Channel[T]) Close(cause error) {
-	c.write.Do(func() {
-		c.cancel(cause)
-	})
+	c.cancel(cause)
 }
 
 // Done reports whether Recv has completed.
 func (c *Channel[T]) Done() bool {
-	return c.done.Load()
+	return c.p.Load() != c.zero
 }
