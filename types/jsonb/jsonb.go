30c0abcfa5bc89995cc872dda0014bcc0e82c219
Alex Tan Hong Pin <alextan220990@gmail.com>
2026-09-06 10:34:14 +0800

    types/jsonb: add setter

--- types/jsonb/jsonb.go
+++ types/jsonb/jsonb.go
@@ -2,15 +2,59 @@ package jsonb
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
+	"slices"
 	"strings"
 )
 
-var ErrSkip = errors.New("skip")
+var (
+	ErrSkip = errors.New("skip")
+	ErrType = errors.New("type")
+)
 
 type Path []string
 
+func (p Path) Match(pattern string) bool {
+	paths := strings.Split(pattern, ".")
+	if len(paths) != len(p) {
+		return false
+	}
+	for i := range len(paths) {
+		want := paths[i]
+		got := p[i]
+		// Match wildcard.
+		if want == "*" {
+			continue
+		}
+		// Check equality.
+		if want != got {
+			return false
+		}
+	}
+	return true
+}
+
 func (p Path) String() string {
 	return strings.Join(p, ".")
+}
+
+func (p Path) AsArrayIndex(i int) Path {
+	if len(p) > 0 {
+		path := slices.Clone(p)
+		path[len(path)-1] += fmt.Sprintf("[%d]", i)
+		return path
+	}
+	return Path{fmt.Sprintf("[%d]", i)}
+}
+
+func (p Path) AsArray() Path {
+	if len(p) > 0 {
+		path := slices.Clone(p)
+		path[len(path)-1] += "[]"
+		return path
+	}
+	return Path{"[]"}
 }
 
 func (p Path) Base() (string, bool) {
@@ -20,40 +64,75 @@ func (p Path) Base() (string, bool) {
 	return "", false
 }
 
-func Reviver(a any, fn func(path Path, value any) error) error {
-	var next func(Path, any) error
-	next = func(paths Path, a any) error {
+func Set(a any, key string, value any) error {
+	typ := reflect.TypeOf(value)
+	return Reviver(a, func(path Path, val any) (any, error) {
+		if path.Match(key) {
+			if reflect.TypeOf(val) == typ {
+				return value, nil
+			}
+			return nil, fmt.Errorf("reviver: %w: cannot set %T to key %s of type %T", ErrType, value, key, val)
+		}
+		return val, nil
+	})
+}
+
+func Get(a any, key string) ([]any, error) {
+	var res []any
+	err := Reviver(a, func(path Path, value any) (any, error) {
+		if path.Match(key) {
+			res = append(res, value)
+		}
+		return value, nil
+	})
+
+	return res, err
+}
+
+func Reviver(a any, fn func(path Path, value any) (any, error)) error {
+	var next func(Path, any) (any, error)
+	next = func(path Path, a any) (any, error) {
 		switch m := a.(type) {
 		case []any:
-			if len(paths) > 0 {
-				last := paths[len(paths)-1]
-				paths[len(paths)-1] = last + "[]"
-			}
-			for _, v := range m {
-				if err := next(paths, v); err != nil {
-					if errors.Is(err, ErrSkip) {
-						return nil
-					}
-					return err
+			arr := path.AsArray()
+			for i, v := range m {
+				n, err := next(arr, v)
+				if err != nil {
+					return m, err
 				}
+				m[i] = n
+
+				o, err := next(path.AsArrayIndex(i), n)
+				if err != nil {
+					return m, err
+				}
+				m[i] = o
 			}
-			return nil
+
+			return fn(path, m)
 		case map[string]any:
 			for k, v := range m {
-				nk := append(paths, k)
-				if err := next(nk, v); err != nil {
-					if errors.Is(err, ErrSkip) {
-						return nil
-					}
-					return err
+				nk := append(path, k)
+				nv, err := next(nk, v)
+				if err != nil {
+					return m, err
+				}
+				if nv == nil {
+					delete(m, k)
+				} else {
+					m[k] = nv
 				}
 			}
 
-			return nil
+			return fn(path, m)
 		default:
-			return fn(paths, a)
+			return fn(path, a)
 		}
 	}
 
-	return next(nil, a)
+	a, err := next(nil, a)
+	if errors.Is(err, ErrSkip) {
+		return nil
+	}
+	return err
 }
--- types/jsonb/jsonb_test.go
+++ types/jsonb/jsonb_test.go
@@ -1,25 +1,106 @@
 package jsonb_test
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
 
 	"github.com/alextanhongpin/core/types/jsonb"
 )
 
-func TestJSONB(t *testing.T) {
-	b := []byte(`{"foo": "bar", "tags": [{"foo": "bar"}, {"foo": "baz"}], "a": "b"}`)
-	var a any
-	err := json.Unmarshal(b, &a)
-	if err != nil {
-		t.Fatal(err)
+func TestJSONB_ReviverGet(t *testing.T) {
+	m := map[string]any{
+		"key1": map[string]any{
+			"key2": map[string]any{
+				"strings": []any{"foo", "bar", "baz"},
+				"maps": []any{
+					map[string]any{
+						"key3": "val3",
+					},
+				},
+			},
+			"key4": "value4",
+		},
 	}
-	err = jsonb.Reviver(a, func(path jsonb.Path, value any) error {
+	err := jsonb.Reviver(m, func(path jsonb.Path, value any) (any, error) {
 		fmt.Println(path.String(), value)
-		return nil
+		return value, nil
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestJSONB_ReviverSet(t *testing.T) {
+	m := map[string]any{
+		"key1": map[string]any{
+			"key2": map[string]any{
+				"strings": []any{"foo", "bar", "baz"},
+				"maps": []any{
+					map[string]any{
+						"key3": "val3",
+					},
+				},
+			},
+			"key4": "value4",
+		},
+	}
+	err := jsonb.Reviver(m, func(path jsonb.Path, value any) (any, error) {
+		if path.Match("key1.key2.maps[].key3") {
+			return "value3", nil
+		}
+		return value, nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(m)
+}
+
+func TestJSONB_Get(t *testing.T) {
+	m := map[string]any{
+		"key1": map[string]any{
+			"key2": map[string]any{
+				"strings": []any{"foo", "bar", "baz"},
+				"maps": []any{
+					map[string]any{
+						"key3": "val3",
+					},
+				},
+			},
+			"key4": "value4",
+		},
+	}
+	res, err := jsonb.Get(m, "key1.key2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(res)
+}
+
+func TestJSONB_Set(t *testing.T) {
+	m := map[string]any{
+		"key1": map[string]any{
+			"key2": map[string]any{
+				"strings": []any{"foo", "bar", "baz"},
+				"maps": []any{
+					map[string]any{
+						"key3": "val3",
+					},
+				},
+			},
+			"key4": "value4",
+		},
+	}
+	err := jsonb.Set(m, "key1.key2.strings", []any{1, 2, 3})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(m)
+
+	err = jsonb.Set(m, "key1.key2.strings", "string")
+	if !errors.Is(err, jsonb.ErrType) {
+		t.Fatal(err)
+	}
+	t.Log(m)
 }
